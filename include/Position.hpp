32ec8c94409b427ae9d5812a526c0e7411688b79
Guilhem Smith <gsmith@student.42.fr>
2019-11-27 12:07:19 +0100

    Common interface for the lib.

---
+++ include/Position.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Position.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/27 11:39:21 by gsmith            #+#    #+#             */
+/*   Updated: 2019/11/27 12:06:19 by gsmith           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef POSITION_HPP
+# define POSITION_HPP
+
+class Position {
+public:
+	Position(size_t x, size_t y);
+	Position(Position const & rhs);
+	~Position(void);
+	Position const &	operator=(Position const & rhs);
+
+	size_t				get_x(void) const;
+	size_t				get_y(void) const;
+	void				set_x(size_t x);
+	void				set_y(size_t y);
+
+private:
+	Position(void);
+
+	size_t				x;
+	size_t				y;
+};
+
+#endif
