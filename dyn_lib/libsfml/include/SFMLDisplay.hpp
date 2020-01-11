675235a90c28ab2ec639be157d6f251a10584097
Timothee BEHRA <tbehra42@gmail.com>
2020-01-11 17:48:50 +0100

    sprites for the snake body, head and tail, mobility and eating not well managed yet

--- dyn_lib/libsfml/include/SFMLDisplay.hpp
+++ dyn_lib/libsfml/include/SFMLDisplay.hpp
@@ -6,7 +6,7 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 17:20:13 by tbehra            #+#    #+#             */
-/*   Updated: 2020/01/07 15:24:54 by tbehra           ###   ########.fr       */
+/*   Updated: 2020/01/11 16:33:29 by tbehra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,75 @@
 
 # define WIDTH_CELL 20
 # define HEIGHT_CELL 20
+# define WIDTH_CELL_F 20.0
+# define HEIGHT_CELL_F 20.0
+# define ORIG_SPRITE_SIZE 64
+# define ORIG_SPRITE_SIZE_F 64.0
+
+
+enum 					draw_state {
+	DRAW_TAIL,
+	
+};
 
 class	SFMLDisplay: public IDisplay {
 public:
+	enum	spriteMotif {
+		obstacle,
+		apple,
+		blueberry,
+		snakeHorizontal,
+		snakeVertical,
+		snakeHeadTop,
+		snakeHeadBottom,
+		snakeHeadLeft,
+		snakeHeadRight,
+		snakeTailTop,
+		snakeTailBottom,
+		snakeTailLeft,
+		snakeTailRight,
+		snakeBendTopLeft,
+		snakeBendTopRight,
+		snakeBendBottomLeft,
+		snakeBendBottomRight
+	};
+
 	SFMLDisplay(void);
  	~SFMLDisplay(void);
 
-	void				newWindow(size_t x, size_t y);
-	void				clearDisplay(void);
-	void				refreshDisplay(void);
-	void				drawStatic(Position & pos, EMotif motif);
-	void				drawMobile(Position & pos, Direction & dest, \
-							Direction & from, EMotif motif, float progression);
-	void				drawScore(int score);
-	EEvent 				pollEvent(void);
+	void						newWindow(size_t x, size_t y);
+	void						clearDisplay(void);
+	void						refreshDisplay(void);
+	void						drawStatic(Position & pos, EMotif motif);
+	void						drawMobile(Position & pos, Direction & dest, \
+								Direction & from, EMotif motif, float progression);
+	void						drawScore(int score);
+	EEvent 						pollEvent(void);
 
 private:
 	SFMLDisplay(SFMLDisplay const &src);
-	SFMLDisplay 		&operator=(SFMLDisplay const &rhs);
-	sf::RenderWindow	*_window;
-	sf::Font			_font;
-	int					_score_offset;
+	SFMLDisplay 				&operator=(SFMLDisplay const &rhs);
+	sf::RenderWindow			*_window;
+	sf::Font					_font;
+	int							_scoreOffset;
+	bool						_spritesAvailable;
+	bool						_drawTail;
+	sf::Texture					_spritesArray;
 	
+	sf::Sprite					*tryGetSpriteStatic(EMotif motif);
+	sf::Sprite					*tryGetSpriteMobile(Direction & dir,
+									Direction & from, EMotif motif);
+	bool						isMobileSnakePosition(EMotif motif);
+	std::pair<size_t, size_t>	getMobileSpritePosition(Direction & dir,
+									Direction & from, EMotif motif);	
+
+
 	const static std::map<sf::Keyboard::Key, IDisplay::EEvent> keyboardToEvent;
 	const static std::map<IDisplay::EMotif, sf::Color> motifToColor;
+	const static std::map<IDisplay::EMotif, std::pair<size_t, size_t>>
+		motifToStaticSpritePosition;
+	const static std::map<spriteMotif, std::pair<size_t, size_t>>
+		spriteMotifToSpritePosition;
 };
 
 extern "C" {
--- dyn_lib/libsfml/src/SFMLDisplay.cpp
+++ dyn_lib/libsfml/src/SFMLDisplay.cpp
@@ -6,21 +6,26 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 17:27:47 by tbehra            #+#    #+#             */
-/*   Updated: 2020/01/07 15:27:27 by tbehra           ###   ########.fr       */
+/*   Updated: 2020/01/11 17:47:12 by tbehra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "SFMLDisplay.hpp"
 
 SFMLDisplay::SFMLDisplay(void): _window(NULL) {
+	_spritesAvailable =
+		_spritesArray.loadFromFile("./dyn_lib/libsfml/images/sprites.png");
+	_drawTail = true;
 }
 
 SFMLDisplay::~SFMLDisplay(void) {
 	if (_window != NULL) {
 		std::cout << "delete sfml window in destructor" << std::endl;
+		_window->setVisible(false);
 		_window->close();
 		delete _window;
-	} }
+	}
+}
 
 const std::map<sf::Keyboard::Key, IDisplay::EEvent> SFMLDisplay::keyboardToEvent = {
 	{sf::Keyboard::Num1, IDisplay::EEvent::One},
@@ -39,6 +44,36 @@ const std::map<IDisplay::EMotif, sf::Color> SFMLDisplay::motifToColor = {
 	{IDisplay::EMotif::obstacle, sf::Color::Cyan},
 	{IDisplay::EMotif::apple, sf::Color::Green},
 	{IDisplay::EMotif::blueberry, sf::Color::Magenta},
+};
+
+const std::map<IDisplay::EMotif, std::pair<size_t, size_t>>
+		SFMLDisplay::motifToStaticSpritePosition = {
+	{IDisplay::EMotif::snake, std::pair<size_t, size_t>(1, 0)},
+	{IDisplay::EMotif::snakeHead, std::pair<size_t, size_t>(3, 0)},
+	{IDisplay::EMotif::obstacle, std::pair<size_t, size_t>(2, 3)},
+	{IDisplay::EMotif::apple, std::pair<size_t, size_t>(0, 3)},
+	{IDisplay::EMotif::blueberry, std::pair<size_t, size_t>(1, 3)},
+};
+
+const std::map<SFMLDisplay::spriteMotif, std::pair<size_t, size_t>>
+		SFMLDisplay::spriteMotifToSpritePosition = {
+	{SFMLDisplay::spriteMotif::obstacle, std::pair<size_t, size_t>(2, 3)},
+	{SFMLDisplay::spriteMotif::apple, std::pair<size_t, size_t>(0, 3)},
+	{SFMLDisplay::spriteMotif::blueberry, std::pair<size_t, size_t>(1, 3)},
+	{SFMLDisplay::spriteMotif::snakeHorizontal, std::pair<size_t, size_t>(1, 0)},
+	{SFMLDisplay::spriteMotif::snakeVertical, std::pair<size_t, size_t>(2, 1)},
+	{SFMLDisplay::spriteMotif::snakeHeadTop, std::pair<size_t, size_t>(3, 0)},
+	{SFMLDisplay::spriteMotif::snakeHeadBottom, std::pair<size_t, size_t>(4, 1)},
+	{SFMLDisplay::spriteMotif::snakeHeadLeft, std::pair<size_t, size_t>(3, 1)},
+	{SFMLDisplay::spriteMotif::snakeHeadRight, std::pair<size_t, size_t>(4, 0)},
+	{SFMLDisplay::spriteMotif::snakeTailTop, std::pair<size_t, size_t>(3, 2)},
+	{SFMLDisplay::spriteMotif::snakeTailBottom, std::pair<size_t, size_t>(4, 3)},
+	{SFMLDisplay::spriteMotif::snakeTailLeft, std::pair<size_t, size_t>(3, 3)},
+	{SFMLDisplay::spriteMotif::snakeTailRight, std::pair<size_t, size_t>(4, 2)},
+	{SFMLDisplay::spriteMotif::snakeBendTopLeft, std::pair<size_t, size_t>(0, 0)},
+	{SFMLDisplay::spriteMotif::snakeBendTopRight, std::pair<size_t, size_t>(2, 0)},
+	{SFMLDisplay::spriteMotif::snakeBendBottomLeft, std::pair<size_t, size_t>(0, 1)},
+	{SFMLDisplay::spriteMotif::snakeBendBottomRight, std::pair<size_t, size_t>(2, 2)},
 };
 
 IDisplay *createDisplay(void) {
@@ -62,7 +97,7 @@ void	SFMLDisplay::newWindow(size_t x, size_t y) {
 	_window = new sf::RenderWindow(
 			sf::VideoMode(WIDTH_CELL * x, HEIGHT_CELL * (y + 1) + 10),
 			"Nibbler - SFML");
-	_score_offset = HEIGHT_CELL * y;
+	_scoreOffset = HEIGHT_CELL * y;
 	_window->setPosition(sf::Vector2i(100, 0));
 	_window->clear(sf::Color::Black);
 	_window->display();
@@ -76,40 +111,170 @@ void	SFMLDisplay::clearDisplay(void) {
 	_window->clear(sf::Color::Black);
 }
 
+sf::Sprite *SFMLDisplay::tryGetSpriteStatic(EMotif motif) {
+	if (!_spritesAvailable)
+		return (NULL);
+	try {
+		std::pair<size_t, size_t> spritePos = motifToStaticSpritePosition.at(motif);
+		return (new sf::Sprite(_spritesArray, 
+			sf::IntRect(ORIG_SPRITE_SIZE * std::get<0>(spritePos),
+				ORIG_SPRITE_SIZE * std::get<1>(spritePos),
+				ORIG_SPRITE_SIZE, ORIG_SPRITE_SIZE)));
+	}
+	catch (std::exception e) {
+		return (NULL);
+	}
+	return (NULL);
+}
+
 void	SFMLDisplay::drawStatic(Position & pos, EMotif motif) {
 	sf::Color color;
+	sf::Sprite *sprite;
 
+	sprite = tryGetSpriteStatic(motif);
+	if (sprite != NULL) {
+		sprite->setScale(sf::Vector2f(WIDTH_CELL_F/ORIG_SPRITE_SIZE_F,
+					HEIGHT_CELL_F/ORIG_SPRITE_SIZE_F));
+		sprite->setPosition(pos.x * WIDTH_CELL, pos.y * HEIGHT_CELL);
+		sprite->setScale(sf::Vector2f(WIDTH_CELL_F/ORIG_SPRITE_SIZE_F,
+					HEIGHT_CELL_F/ORIG_SPRITE_SIZE_F));
+		_window->draw(*sprite);
+		delete sprite;
+	}
+	else {
+		try {
+			color = motifToColor.at(motif);
+		}
+		catch (std::out_of_range oor) {
+			color = sf::Color::Black;
+		}
+		sf::RectangleShape toDraw(sf::Vector2f(WIDTH_CELL, HEIGHT_CELL));
+		toDraw.setFillColor(color);
+		toDraw.setPosition(pos.x * WIDTH_CELL, pos.y * HEIGHT_CELL);
+		_window->draw(toDraw);
+	}
+}
+
+std::pair<size_t, size_t> SFMLDisplay::getMobileSpritePosition(Direction & dir,
+		Direction & from, EMotif motif) {
+	SFMLDisplay::spriteMotif res;
+
+	if (motif == IDisplay::EMotif::snakeHead) {
+		if (dir.x == 1 && dir.y == 0) 
+			res = snakeHeadRight;
+		else if (dir.x == -1 && dir.y == 0)
+			res = snakeHeadLeft;
+		else if (dir.x == 0 && dir.y == -1)
+			res = snakeHeadTop;
+		else if (dir.x == 0 && dir.y == 1)
+			res = snakeHeadBottom;
+		_drawTail = true;
+	}
+	else if (motif == IDisplay::EMotif::snake) {
+		if (_drawTail) {
+			if (dir.x == 0 && dir.y == 1) 
+				res = snakeTailBottom;
+			else if (dir.x == 0 && dir.y == -1)
+				res = snakeTailTop;
+			else if (dir.x == -1 && dir.y == 0)
+				res = snakeTailLeft;
+			else if (dir.x == 1 && dir.y == 0)
+				res = snakeTailRight;
+			_drawTail = false;
+		}
+		else if (from.x != dir.x || from.y != dir.y)
+		{
+			if ((from.x == -1 && dir.y == 1) || (from.y == -1 && dir.x == 1))
+				res = snakeBendTopLeft;
+			else if ((from.y == -1 && dir.x == -1) || (from.x == 1 && dir.y == 1))
+				res = snakeBendTopRight;
+			else if ((from.x == 1 && dir.y == -1) || (from.y == 1 && dir.x == -1))
+				res = snakeBendBottomRight;
+			else
+				res = snakeBendBottomLeft;
+		}
+		else {
+			res = (dir.x == 0) ? snakeVertical : snakeHorizontal;
+		}
+	}
+	else {
+		if (motif == IDisplay::EMotif::obstacle) 
+			res = obstacle;
+		else if (motif == IDisplay::EMotif::apple) 
+			res = apple;
+		else if (motif == IDisplay::EMotif::blueberry) 
+			res = blueberry;
+	}
+	return spriteMotifToSpritePosition.at(res);
+}
+
+sf::Sprite *SFMLDisplay::tryGetSpriteMobile(Direction & dir,
+		Direction & from, EMotif motif) {
+	if (!_spritesAvailable)
+		return (NULL);
 	try {
-		color = motifToColor.at(motif);
+		std::pair<size_t, size_t> spritePos = getMobileSpritePosition(dir, from,
+				motif);
+		return (new sf::Sprite(_spritesArray, 
+			sf::IntRect(ORIG_SPRITE_SIZE * (std::get<0>(spritePos)),
+				ORIG_SPRITE_SIZE * (std::get<1>(spritePos)),
+				ORIG_SPRITE_SIZE, ORIG_SPRITE_SIZE)));
 	}
-	catch (std::out_of_range oor) {
-		color = sf::Color::Black;
+	catch (std::exception e) {
+		return (NULL);
 	}
-	sf::RectangleShape toDraw(sf::Vector2f(WIDTH_CELL, HEIGHT_CELL));
-	toDraw.setFillColor(color);
-	toDraw.setPosition(pos.x * WIDTH_CELL, pos.y * HEIGHT_CELL);
-	_window->draw(toDraw);
+	return (NULL);
+}
+
+bool	SFMLDisplay::isMobileSnakePosition(EMotif motif) {
+	if (_drawTail) {
+		return (true);
+	}
+	return (motif == IDisplay::EMotif::snakeHead);
 }
 
 void	SFMLDisplay::drawMobile(Position & pos, Direction & dest, \
 					Direction & from, EMotif motif, float progression)
 {
-	(void)from;
 	sf::Color color;
+	sf::Sprite *sprite;
 
-	try {
-		color = motifToColor.at(motif);
+	sprite = tryGetSpriteMobile(dest, from, motif);
+	if (sprite != NULL) {
+		sprite->setScale(sf::Vector2f(WIDTH_CELL_F/ORIG_SPRITE_SIZE_F,
+					HEIGHT_CELL_F/ORIG_SPRITE_SIZE_F));
+		if (isMobileSnakePosition(motif)) {
+			sprite->setPosition(
+				pos.x * WIDTH_CELL + dest.x * progression * WIDTH_CELL,
+				pos.y * HEIGHT_CELL + dest.y * progression * HEIGHT_CELL);
+		}
+		else {
+			sprite->setPosition(
+				pos.x * WIDTH_CELL + dest.x,
+				pos.y * HEIGHT_CELL + dest.y);
+		}
+		sprite->setScale(sf::Vector2f(WIDTH_CELL_F/ORIG_SPRITE_SIZE_F,
+					HEIGHT_CELL_F/ORIG_SPRITE_SIZE_F));
+		_window->draw(*sprite);
+		delete sprite;
 	}
-	catch (std::out_of_range oor) {
-		color = sf::Color::Black;
+	else {
+		try {
+			color = motifToColor.at(motif);
+		}
+		catch (std::out_of_range oor) {
+			color = sf::Color::Black;
+		}
+		sf::RectangleShape toDraw(sf::Vector2f(WIDTH_CELL, HEIGHT_CELL));
+		toDraw.setFillColor(color);
+		toDraw.setPosition(
+			static_cast<int>(pos.x * WIDTH_CELL
+				+ dest.x * progression * WIDTH_CELL),
+			static_cast<int>(pos.y * HEIGHT_CELL
+				+ dest.y * progression * HEIGHT_CELL)
+			);
+		_window->draw(toDraw);
 	}
-	sf::RectangleShape toDraw(sf::Vector2f(WIDTH_CELL, HEIGHT_CELL));
-	toDraw.setFillColor(color);
-	toDraw.setPosition(
-		static_cast<int>(pos.x * WIDTH_CELL + dest.x * progression * WIDTH_CELL),
-		static_cast<int>(pos.y * HEIGHT_CELL + dest.y * progression * HEIGHT_CELL)
-	);
-	_window->draw(toDraw);
 }	
 
 void	SFMLDisplay::drawScore(int score) {
@@ -118,7 +283,7 @@ void	SFMLDisplay::drawScore(int score) {
 	text.setString("Score: " + std::to_string(score));
 	text.setCharacterSize(HEIGHT_CELL);
 	text.setFillColor(sf::Color::White);
-	text.setPosition(10, _score_offset);
+	text.setPosition(10, _scoreOffset);
 	_window->draw(text);
 }
 
@@ -133,6 +298,13 @@ IDisplay::EEvent SFMLDisplay::pollEvent(void) {
 		}
 		if (event.type == sf::Event::KeyPressed) {
 			try {
+				if (event.key.code == sf::Keyboard::Num3)
+				{
+					// doesn't work ! Find why
+					std::cout << "here"<< std::endl;
+					_window->setVisible(false);
+				}
+
 				return this->keyboardToEvent.at(event.key.code);
 			}
 			catch (std::out_of_range &oor) {
